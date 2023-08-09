151a324da64a4145e3fb6de507d49dc278c240c8
Vaibhav ka ultroid <97387093+Awesome-Vaibhav@users.noreply.github.com>
2023-08-09 21:54:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 123))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 1747534))
+    API_HASH = os.environ.get("API_HASH", "5a2684512006853f2e48aca9652d83ea")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5457463964:AAHvbTRsUIU4KTNGlOVDdF1jkXSSmDqEEcg")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     TG_MAX_SIZE = 2040108421
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1316963576))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1144738419))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 
